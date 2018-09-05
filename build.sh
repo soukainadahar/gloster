c3c3d73d5de1eaac46e74636c5aa9bd5a2cdd096
soukaina <soukainadahar@gmail.com>
2018-09-05 08:41:09 +0100

    Executable!

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
-    ...
-elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
-      echo 'Build and analyze pull request'
-      mvn -B clean verify sonar:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=skokaina/devops -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 
+'develop' ]]; then
+      echo 'Checking Quality Gates'
+      mvn -B clean verify sonar:sonar \
+      -Dsonar.host.url=${SONAR_URL} \
+      -Dsonar.login=${SONAR_LOGIN} \
+      -Dsonar.projectKey=${SONAR_PROJECT} \
+      -Dsonar.organization=${SONAR_ORGANIZATION}
 fi
