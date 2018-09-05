ced46866038bbf7a800a53a120d8fa1bee2dc3a2
soukainadahar <42774770+soukainadahar@users.noreply.github.com>
2018-09-05 09:06:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 
-'develop' ]]; then
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
       echo 'Checking Quality Gates'
       mvn -B clean verify sonar:sonar \
       -Dsonar.host.url=${SONAR_URL} \
       -Dsonar.login=${SONAR_LOGIN} \
       -Dsonar.projectKey=${SONAR_PROJECT} \
       -Dsonar.organization=${SONAR_ORGANIZATION}
+
+elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
+      echo 'Build and analyze pull request'
+      mvn -B clean verify sonar:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=soukainadahar/gloster -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
 fi
